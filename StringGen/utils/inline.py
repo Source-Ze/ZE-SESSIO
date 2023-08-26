42da5a773a933661f97cecd013561217de62a2c8
Zeinn0 <127055306+Zeinn0@users.noreply.github.com>
2023-08-26 17:57:33 +0300

    Add files via upload

---
+++ StringGen/utils/inline.py
@@ -0,0 +1,30 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import SUPPORT_CHAT
+
+
+keyboard = InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton(text="» ⚡ ¦ بـدء إنـشـاء جـلسـة ", callback_data="gensession")],
+        [
+            InlineKeyboardButton(text="جرۆبْ آلُدِعم", url=SUPPORT_CHAT),
+            InlineKeyboardButton(
+                text="●○𝚂𝙴𝚉𝙰𝚁○●", url="https://t.me/UIU_II"
+            ),
+        ],
+    ]
+)
+
+gen_key = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(text="🎙 بـايـࢪوجـࢪام 🎙", callback_data="pyrogram1"),
+            InlineKeyboardButton(text="🎙 بـايـࢪوجـࢪام v2🎙", callback_data="pyrogram"),
+        ],
+        [InlineKeyboardButton(text="🎛 تـلـيـثـونـ 🎛", callback_data="telethon")],
+    ]
+)
+
+retry_key = InlineKeyboardMarkup(
+    [[InlineKeyboardButton(text="ᴛʀʏ ᴀɢᴀɪɴ", callback_data="gensession")]]
+)
